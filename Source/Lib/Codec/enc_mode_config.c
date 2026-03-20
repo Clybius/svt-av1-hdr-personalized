87b90c34a76bab871ba8cb9f471529021f074fe7
Clybius <clybius@gmail.com>
2026-03-20 13:57:55 -0500

    Feat: Add --enable-daala with values 0 (default) through 3 (fully enabled)

--- Source/Lib/Codec/enc_mode_config.c
+++ Source/Lib/Codec/enc_mode_config.c
@@ -7098,7 +7098,8 @@ void svt_aom_sig_deriv_enc_dec(SequenceControlSet *scs, PictureControlSet *pcs, 
         skip_sub_depth_lvl = 2;
 
     set_skip_sub_depth_ctrls(&ctx->skip_sub_depth_ctrls, skip_sub_depth_lvl);
-    ctx->tune_ssim_level = SSIM_LVL_0;
+    ctx->tune_ssim_level  = SSIM_LVL_0;
+    ctx->tune_daala_level = scs->static_config.enable_daala;
 }
 /*
 * return the 4x4 level
